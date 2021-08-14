76acd05126c754837bccc1f9fcaf41f775160653
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-08-14 22:44:53 +0200

    recreate the app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Stack from './components/Stack';
+import Work from './components/Work';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Header />
+      <Work />
+      <Stack />
+      <Contact />
     </div>
   );
 }
